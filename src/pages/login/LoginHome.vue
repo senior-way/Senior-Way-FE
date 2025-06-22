0686ac84f4e7de0ede5336b4f064af577defe86f
Lee Dayeon <carrotday0429@gmail.com>
2025-06-22 15:13:09 +0900

    Feat: improve overall UI logo, fonts, and styles updated

--- src/pages/login/LoginHome.vue
+++ src/pages/login/LoginHome.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="wrap">
-    <header><p>SENIORWAY</p></header>
+    <header><img src="@/assets/img/seniorway_logo.png" alt="SENIORWAY Logo" class="header_logo" /></header>
     <div class="container">
       <div class="content">
         <div class="register_btn_wrap">
           <button class="large_btn" @click="goToRegister">
-            <p class="main">회원가입 하기</p>
+            <p class="main">회원가입</p>
             <p class="sub">처음 방문했어요!</p>
           </button>
         </div>
         <div class="login_btn_wrap">
           <button class="large_btn" @click="goToLogin">
-            <p class="main">로그인 하기</p>
+            <p class="main">로그인</p>
             <p class="sub">일반 계정이 있어요!</p>
           </button>
         </div>
@@ -28,8 +28,10 @@
     </footer>
   </div>
 </template>
+
+
 <script setup>
-import kakaoLoginBtn from '@/assets/kakao_login.png';
+import kakaoLoginBtn from '@/assets/img/kakao_login.png';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -42,16 +44,26 @@ const goToRegister = () => {
   router.push('/register');
 };
 </script>
+
+
 <style scoped>
 * {
   margin: 0;
   padding: 0;
   line-height: 1;
-  letter-spacing: -0.03rem;
   font-weight: 500;
 }
+
 /* header */
-header {
+.header_logo {
+  padding-top: 2rem;
+  padding-bottom: 5rem;
+  width: 15rem; 
+  display: block; 
+  margin: 0 auto;
+}
+
+/* header {
   text-align: center;
   font-size: 2rem;
   font-weight: 700;
@@ -60,9 +72,11 @@ header {
   letter-spacing: 2px;
   margin-top: 2.59375rem;
   margin-bottom: 4.28125rem;
-}
+} */
+
 /* footer */
 footer {
+  font-family: 'Font-Medium';
   font-size: 1rem;
   color: #7d828d;
   text-decoration: underline;
@@ -71,6 +85,7 @@ footer {
   align-items: center;
   margin-bottom: 2.5625rem;
 }
+
 /* class */
 .wrap {
   width: 100%;
@@ -83,50 +98,61 @@ footer {
   display: flex;
   background-color: #f6f6f6;
 }
+
 .container {
   flex-grow: 1;
 }
+
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+
 .large_btn {
   width: 250px;
   height: 150px;
   border: 0 solid #ffffff;
   border-radius: 0.75rem;
   background-color: #ffffff;
-  margin-bottom: 1.125rem;
+  margin-bottom: 1.8rem;
 }
+
 .main {
+  font-family: 'Font-Bold';
   font-size: 1.75rem;
-  font-weight: 700;
   height: 1.75rem;
 }
+
 .sub {
+  font-family : 'Font-Medium';
   font-size: 1rem;
   color: #7d828d;
   margin-top: 1.8125rem;
 }
+
 .kakao_login_btn_wrap {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+
 .kakao_login_btn {
   width: 244px;
   height: 60px;
   border: none;
   cursor: pointer;
 }
+
 .kakao_login_btn img {
   width: 100%;
   height: 100%;
   display: block;
 }
+
 .kakao_login_text {
   margin-top: 1.0625rem;
+  font-family: 'Font-Medium';
   font-size: 1rem;
   color: #7d828d;
 }
