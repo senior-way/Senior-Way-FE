db24b627e3ce21a543d893accc0bb5e3e8c58da4
Lee Dayeon <carrotday0429@gmail.com>
2025-08-29 19:21:38 +0900

    chore - update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-import './assets/fonts/fonts.css';
+import '@/assets/main.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
