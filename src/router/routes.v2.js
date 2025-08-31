78f28817cf8e345c6e592f54504b649566af6cdf
Lee Dayeon <carrotday0429@gmail.com>
2025-08-31 17:35:40 +0900

    feat #30 - 로그인 페이지 구현

--- src/router/routes.v2.js
+++ src/router/routes.v2.js
@@ -1,6 +1,38 @@
+// router.v2.js
 import ErrorPage from '@/newpages/home/ErrorPage.vue'
 
 const routesV2 = [
+  // /v2 로그인
+  {
+    path: '/v2',
+    redirect: { name: 'LoginV2' },
+  },
+
+  // 로그인
+  {
+    path: '/v2/login',
+    name: 'LoginV2',
+    component: () => import('@/newpages/auth/LoginPage.vue'),
+    meta: { title: '로그인' },
+  },
+
+  // Auth 홈
+  {
+    path: '/v2/auth',
+    name: 'AuthHomeV2',
+    component: () => import('@/newpages/auth/AuthHomePage.vue'),
+    meta: { title: 'Auth' },
+  },
+
+  // 회원가입
+  {
+    path: '/v2/signin',
+    name: 'SigninV2',
+    component: () => import('@/newpages/auth/SigninPage.vue'),
+    meta: { title: '회원가입' },
+  },
+
+  // 404
   {
     path: '/v2/:pathMatch(.*)*',
     name: 'NotFoundV2',
