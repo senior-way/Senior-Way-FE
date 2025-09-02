8631d968461eb2c02c33c7817eeaa3f1b8cb43fa
Lee Dayeon <carrotday0429@gmail.com>
2025-09-02 22:24:12 +0900

    feat #32 - db.json,json server 설정

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
   server: {
-    host: true, // ← 이게 핵심!
-    port: 5173, // 기본값 (원하면 바꿔도 됨)
-  },
-});
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: p => p.replace(/^\/api/, '')
+      }
+    }
+  }
+})
