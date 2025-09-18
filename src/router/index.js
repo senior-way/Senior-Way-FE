3ac612ab10895464b3873a1ef8353550ba6c8f4d
Jeseung Yoon <109639745+yjs1228@users.noreply.github.com>
2025-09-18 13:36:38 +0900

    Feat: 사용자 설문, 일정 생성 백엔드 연동 구현현 #63

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import routesV1 from './routes.v1'
-import routesV2 from './routes.v2'
+import { createRouter, createWebHistory } from 'vue-router';
+import routesV2 from './routes.v2';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    ...routesV1,
-    ...routesV2,
-  ],
-})
+  routes: [...routesV2],
+});
 
-export default router
+export default router;
